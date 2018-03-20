68f30fd495b94f47a35c481d98ccdc00f2d97c5d
M Siverback <martin_siverback@hotmail.com>
2018-03-20 08:26:30 +0100

    A few steps towards some sort of solution.

--- Main.java
+++ Main.java
@@ -1,14 +1,36 @@
 /******************************************************************************
  * Just some simple main function to get everybody started.
  */
-
+import java.util.*;
 public class Main
 {
+    
+    public static String convertToRoman(Integer decimalNumber)
+    {
+        if (decimalNumber == 4)
+            return "IV";
+        if (decimalNumber == 3)
+            return "III";
+        if (decimalNumber == 2)
+            return "II";
+        else
+            return "I";
+    }
+    
+    
 	public static void main(String[] args) {
+	    HashMap<Integer, String> testData = new HashMap<Integer, String>();
 		TestHelper testhelper = new TestHelper();
-		testhelper.checkTest(1, "II", "I");
-		testhelper.checkTest(1, "I", "I");
-		testhelper.checkTest(3, "II", "III");
+        testData.put(1, "I");
+	    testData.put(2, "II");
+	    testData.put(3, "III");
+	    testData.put(4, "IV");
+        for(HashMap.Entry<Integer, String> e : testData.entrySet())
+        {
+            testhelper.checkTest(e.getKey(), 
+                                 convertToRoman(e.getKey()), 
+                                 e.getValue());
+        }
 		testhelper.endSuite();
 	}
 }
